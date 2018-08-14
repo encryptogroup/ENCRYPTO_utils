9247fc1ec964605f89b51dcb838c0c16ed661649
Lennart Braun <lenerd@posteo.de>
2018-08-14 21:48:05 +0200

    cbitvector: constexpr arrays and move to anon ns
    Make the helper arrays containing bitmasks et al. constexpr and moves
    them into an unamed namespaces. Also move helper templates into the
    unamed namespace to get internal linkage.

--- src/ENCRYPTO_utils/cbitvector.cpp
+++ src/ENCRYPTO_utils/cbitvector.cpp
@@ -24,8 +24,11 @@
 #include <iostream>
 #include <cstring>
 
+
+namespace {
+
 /** Array which stores the bytes which are reversed. For example, the hexadecimal 0x01 is when reversed becomes 0x80.  */
-static const BYTE REVERSE_BYTE_ORDER[256] = { 0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0, 0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8,
+constexpr BYTE REVERSE_BYTE_ORDER[256] = { 0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0, 0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8,
 		0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8, 0x04, 0x84, 0x44, 0xC4, 0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4, 0x0C, 0x8C,
 		0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC, 0x3C, 0xBC, 0x7C, 0xFC, 0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2, 0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2,
 		0x72, 0xF2, 0x0A, 0x8A, 0x4A, 0xCA, 0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA, 0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96,
@@ -40,72 +43,101 @@ static const BYTE REVERSE_BYTE_ORDER[256] = { 0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0
 	This array is used by \link XORBits(BYTE* p, int pos, int len) \endlink and \link SetBits(BYTE* p, uint64_t pos, uint64_t len) \endlink
     method for lower bit mask.
 */
-static const BYTE RESET_BIT_POSITIONS[9] = { 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF };
+constexpr BYTE RESET_BIT_POSITIONS[9] = { 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF };
 /**
 	This array is used by \link XORBits(BYTE* p, int pos, int len) \endlink and \link SetBits(BYTE* p, uint64_t pos, uint64_t len) \endlink
     method for upper bit mask.
 */
-static const BYTE RESET_BIT_POSITIONS_INV[9] = { 0x00, 0x80, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC, 0xFE, 0xFF };
+constexpr BYTE RESET_BIT_POSITIONS_INV[9] = { 0x00, 0x80, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC, 0xFE, 0xFF };
 
 /** This array is used by \link GetBits(BYTE* p, int pos, int len) \endlink method for lower bit mask. */
-static const BYTE GET_BIT_POSITIONS[9] = { 0xFF, 0xFE, 0xFC, 0xF8, 0xF0, 0xE0, 0xC0, 0x80, 0x00 };
+constexpr BYTE GET_BIT_POSITIONS[9] = { 0xFF, 0xFE, 0xFC, 0xF8, 0xF0, 0xE0, 0xC0, 0x80, 0x00 };
 
 /** This array is used by \link GetBits(BYTE* p, int pos, int len) \endlink method for upper bit mask. */
-static const BYTE GET_BIT_POSITIONS_INV[9] = { 0xFF, 0x7F, 0x3F, 0x1F, 0x0F, 0x07, 0x03, 0x01, 0x00 };
+constexpr BYTE GET_BIT_POSITIONS_INV[9] = { 0xFF, 0x7F, 0x3F, 0x1F, 0x0F, 0x07, 0x03, 0x01, 0x00 };
 
 /**
 	This array is used for masking bits and extracting a particular positional bit from the provided byte array.
 	This array is used by \link GetBit(int idx) \endlink method.
 */
-static const BYTE MASK_BIT[8] = { 0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1 };
+constexpr BYTE MASK_BIT[8] = { 0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1 };
 
 /**
 	This array is used for extracting a particular positional bit from the provided byte array without masking.
 	This array is used by \link GetBitNoMask(int idx) \endlink method.
 */
-static const BYTE BIT[8] = { 0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80 };
+static constexpr BYTE BIT[8] = { 0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80 };
 
 /**
 	This array is used for masking bits and setting a particular positional bit from the provided byte array in the CBitVector.
 	This array is used by \link SetBit(int idx, BYTE b) \endlink and \link ANDBit(int idx, BYTE b) \endlink methods.
 */
-static const BYTE CMASK_BIT[8] = { 0x7f, 0xbf, 0xdf, 0xef, 0xf7, 0xfb, 0xfd, 0xfe };
+constexpr BYTE CMASK_BIT[8] = { 0x7f, 0xbf, 0xdf, 0xef, 0xf7, 0xfb, 0xfd, 0xfe };
 
 /**
 	This array is used for setting a particular positional bit from the provided byte array without masking in the CBitVector.
 	This array is used by \link SetBitNoMask(int idx, BYTE b) \endlink and \link ANDBitNoMask(int idx, BYTE b) \endlink methods.
 */
-static const BYTE C_BIT[8] = { 0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF, 0x7F };
+constexpr BYTE C_BIT[8] = { 0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF, 0x7F };
 
 /**
 	This array is used for masking bits and setting a particular positional bit from the provided byte array in the CBitVector.
 	This array is used by \link SetBit(int idx, BYTE b) \endlink and \link XORBit(int idx, BYTE b) \endlink methods.
 */
-static const BYTE MASK_SET_BIT_C[2][8] = { { 0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1 }, { 0, 0, 0, 0, 0, 0, 0, 0 } };
+constexpr BYTE MASK_SET_BIT_C[2][8] = { { 0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1 }, { 0, 0, 0, 0, 0, 0, 0, 0 } };
 
 /**
 	This array is used for setting a particular positional bit from the provided byte array without masking in the CBitVector.
 	This array is used by \link SetBitNoMask(int idx, BYTE b) \endlink and \link XORBitNoMask(int idx, BYTE b) \endlink methods.
 */
-static const BYTE SET_BIT_C[2][8] = { { 0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80 }, { 0, 0, 0, 0, 0, 0, 0, 0 } };
+constexpr BYTE SET_BIT_C[2][8] = { { 0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80 }, { 0, 0, 0, 0, 0, 0, 0, 0 } };
 
 const BYTE SELECT_BIT_POSITIONS[9] = { 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF };
 
 #ifdef MACHINE_SIZE_32
-static const REGISTER_SIZE TRANSPOSITION_MASKS[6] =
+constexpr REGISTER_SIZE TRANSPOSITION_MASKS[6] =
 {	0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF, 0x0000FFFF};
-static const REGISTER_SIZE TRANSPOSITION_MASKS_INV[6] =
+constexpr REGISTER_SIZE TRANSPOSITION_MASKS_INV[6] =
 {	0xAAAAAAAA, 0xCCCCCCCC, 0xF0F0F0F0, 0xFF00FF00, 0xFFFF0000};
 #else
 #ifdef MACHINE_SIZE_64
 /** Transposition mask used for Eklund Bit Matrix Transposition.*/
-static const REGISTER_SIZE TRANSPOSITION_MASKS[6] = { 0x5555555555555555, 0x3333333333333333, 0x0F0F0F0F0F0F0F0F, 0x00FF00FF00FF00FF, 0x0000FFFF0000FFFF, 0x00000000FFFFFFFF };
-static const REGISTER_SIZE TRANSPOSITION_MASKS_INV[6] = { 0xAAAAAAAAAAAAAAAA, 0xCCCCCCCCCCCCCCCC, 0xF0F0F0F0F0F0F0F0, 0xFF00FF00FF00FF00, 0xFFFF0000FFFF0000, 0xFFFFFFFF00000000 };
+constexpr REGISTER_SIZE TRANSPOSITION_MASKS[6] = { 0x5555555555555555, 0x3333333333333333, 0x0F0F0F0F0F0F0F0F, 0x00FF00FF00FF00FF, 0x0000FFFF0000FFFF, 0x00000000FFFFFFFF };
+constexpr REGISTER_SIZE TRANSPOSITION_MASKS_INV[6] = { 0xAAAAAAAAAAAAAAAA, 0xCCCCCCCCCCCCCCCC, 0xF0F0F0F0F0F0F0F0, 0xFF00FF00FF00FF00, 0xFFFF0000FFFF0000, 0xFFFFFFFF00000000 };
 #else
 #endif
 #endif
 
-static const size_t SHIFTVAL = 3;
+constexpr size_t SHIFTVAL = 3;
+
+
+template<class T> void GetBytes(T* dst, const T* src, const T* lim) {
+	while (dst != lim) {
+		*dst++ = *src++;
+	}
+}
+
+template<class T> void SetBytes(T* dst, const T* src, const T* lim) {
+	while (dst < lim) {
+		*dst++ = *src++;
+	}
+}
+
+//Generic bytewise XOR operation
+template<class T> void XORBytes(T* dst, const T* src, const T* lim) {
+	while (dst != lim) {
+		*dst++ ^= *src++;
+	}
+}
+
+template<class T> void ANDBytes(T* dst, const T* src, const T* lim) {
+	while (dst != lim) {
+		*dst++ &= *src++;
+	}
+}
+
+}  // namespace
+
 
 CBitVector::CBitVector() {
 	Init();
@@ -384,12 +416,6 @@ void CBitVector::GetBits(BYTE* p, std::size_t pos, std::size_t len) const {
 }
 
 
-template<class T> void GetBytes(T* dst, const T* src, const T* lim) {
-	while (dst != lim) {
-		*dst++ = *src++;
-	}
-}
-
 //optimized bytewise for set operation
 void CBitVector::GetBytes(BYTE* p, std::size_t pos, std::size_t len) const {
 	assert(pos+len <= m_nByteSize);
@@ -448,12 +474,6 @@ void CBitVector::SetBitsPosOffset(const BYTE* p, std::size_t ppos, std::size_t p
 	}
 }
 
-template<class T> void SetBytes(T* dst, const T* src, const T* lim) {
-	while (dst < lim) {
-		*dst++ = *src++;
-	}
-}
-
 //optimized bytewise for set operation
 void CBitVector::SetBytes(const BYTE *src, std::size_t pos, std::size_t len) {
 	assert(pos + len <= m_nByteSize);
@@ -484,13 +504,6 @@ void CBitVector::SetBitsToZero(std::size_t bitpos, std::size_t bitlen) {
 	}
 	for (std::size_t i = (firstlim + firstlen) << 8; i < bitpos + bitlen; i++) {
 		SetBitNoMask(i, 0);
-	}
-}
-
-//Generic bytewise XOR operation
-template<class T> void XORBytes(T* dst, const T* src, const T* lim) {
-	while (dst != lim) {
-		*dst++ ^= *src++;
 	}
 }
 
@@ -576,12 +589,6 @@ void CBitVector::XORBytesReverse(const BYTE* p, std::size_t pos, std::size_t len
 	}
 }
 
-
-template<class T> void ANDBytes(T* dst, const T* src, const T* lim) {
-	while (dst != lim) {
-		*dst++ &= *src++;
-	}
-}
 
 //optimized bytewise for AND operation
 void CBitVector::ANDBytes(const BYTE* p, std::size_t pos, std::size_t len) {
