e2063e5a2536ca5afc37e8671d5a4864f3ecbfb2
Lennart Braun <lenerd@posteo.de>
2018-08-19 02:39:44 +0200

    test: add test for CBitVector::SetBitsPosOffset
    Thanks to Sebastian Stammler <stammler@cbs.tu-darmstadt.de> for
    providing the test case.

---
+++ test/test_cbitvector.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include "ENCRYPTO_utils/cbitvector.h"
+
+
+TEST(TestCBitVector, SetBitsPosOffset) {
+
+	auto read_bits = [] (const CBitVector& v) {
+		uint16_t bits;
+		v.GetBits(reinterpret_cast<uint8_t*>(&bits), 0, 16);
+		return bits;
+	};
+
+	// creating zero vector
+	CBitVector v;
+	v.CreateZeros(16);
+
+	ASSERT_EQ(read_bits(v), 0b0000000000000000);
+
+	// Setting first two bits
+	v.SetBitNoMask(0, 1);
+	v.SetBitNoMask(1, 1);
+
+	ASSERT_EQ(read_bits(v), 0b0000000000000011);
+
+	// Setting bits 6-9 by other all-one byte array, accessing from bit 3
+	uint8_t all_one[1] = {0xff};
+	v.SetBitsPosOffset(all_one, 3, 6, 4);
+
+	ASSERT_EQ(read_bits(v), 0b0000001111000011);
+
+	// Setting bits 0-5 by other alternating byte array, accessing from bit 2
+	uint8_t alternating[1] = {0b01010101};
+	v.SetBitsPosOffset(alternating, 2, 0, 6);
+
+	ASSERT_EQ(read_bits(v), 0b0000001111010101);
+}
